b17a108dc600026b00abb5f2423349231b9cbbbc
EruditeCoder <marcosaleman84@gmail.com>
2020-09-17 16:34:43 -0400

    Minor changes

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+// Nav Bar (MA (logo) | Projects | About Me | Let's Talk)
+
+function App() {
+    return (
+        <div class="mt-48">
+            <div class="px-2">
+                <div class="flex -mx-2">
+                    <div class="w-2/5 px-2">
+                        <div class="bg-gray-400">
+                            <h2> Hello, I'm </h2>
+                            <h1> Marcos Aleman </h1>
+                            <h2> Full Stack Developer </h2>
+                            Lorem ipsum dolor sit amet, consectetur 
+                            adipiscing elit, sed do eiusmod tempor 
+                            incididunt ut labore et dolore magna aliqua. 
+                            Ut enim ad minim veniam, quis nostrud exercitation 
+                            ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                            Duis aute irure dolor in reprehenderit in voluptate 
+                            velit esse cillum dolore eu fugiat nulla pariatur. 
+                            Excepteur sint occaecat cupidatat non proident, sunt 
+                            in culpa qui officia deserunt mollit anim id est laborum
+                        </div>
+                    </div>
+                    <div class="w-1/5 px-2">
+                        <div class="bg-gray-500">
+                            Filler
+                        </div>
+                    </div>
+                    <div class="w-2/5 px-2">
+                        <div class="bg-gray-400">
+                            List
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default App
