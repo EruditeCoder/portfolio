00a0ecba391326ee8502d87622fa4ce1901e45f0
EruditeCoder <marcosaleman84@gmail.com>
2020-09-20 22:15:24 -0400

    Made list a component

---
+++ src/Components/List.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const List = () => {
+    return (
+        <ul className="text-xl text-center">
+            <li>home</li>
+            <li>about</li>
+            <li>projects</li>
+        </ul>
+    )
+}
+export default List
