bbab94878180ab7f74cd3375ec5e11db0af5fe73
EruditeCoder <marcosaleman84@gmail.com>
2020-09-20 22:06:09 -0400

    Design changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from 'react'
 import Home from './Views/Home'
+import List from './Components/List'
 
 function App() {
     return (
         <div className="mt-48">
             <div className="px-2">
-                <div className="flex -mx-2">
-                    <div className="w-2/6 px-2">
-                        <div className="bg-gray-400">
-                            <Home />
-                        </div>
+
+                <div className="flex">
+
+                    <div className="w-7/12 p-5 text-white">
+                        <Home />
                     </div>
-                    <div className="w-2/6 px-2 pl-10 border-l-2 bg-gray-400">
-                        <div className="bg-blue-400 h-full w-1 justify-center"></div>
+                    
+                    {/* Divider */}
+                    <div className="bg-gray-400 w-px h-auto justify-center mx-6">&nbsp;</div>
+
+                    <div className="w-5/12 p-5 text-white">
+                        <List />
                     </div>
-                    <div className="w-2/6 px-2 pr-10">
-                        <div className="bg-gray-400">
-                            List
-                        </div>
-                    </div>
+
                 </div>
+
             </div>
         </div>
     )
--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,19 +1,21 @@
 import React from 'react'
 
-export default function Home() {
+const Home = () => {
     return (
-        <div className="font-sans">
-            <h2 className="text-2xl"> Hello, I'm </h2>
-            <h1 className="text-3xl"> Marcos Aleman </h1>
-            <h2 className="text-2xl"> Full Stack Developer </h2>
-            <p className="text-xl"> Experienced and knowledgeable 
-            network specialist with 3 years of work experience and 
-            a proven knowledge of network administration, network 
-            security, and computer programming. Aiming to leverage 
-            my abilities to successfully provide effective solutions
-            to complex issues in the field of network engineering 
-            and computer programming.
+        <div>
+            <h2 className="text-xl text-center"> Hello, I'm </h2>
+            <h1 className="text-3xl text-center"> MARCOS ALEMAN </h1>
+            <h2 className="text-2xl text-center"> Full Stack Developer </h2>
+            <p className="text-xl text-center"> Network specialist with 3 years of work experience and 
+            a proven knowledge of network administration, security, and programming.
             </p>
+            {/* <p className="text-2xl"> Aiming to leverage my abilities 
+            to successfully provide effective solutions to complex 
+            issues in the field of network engineering 
+            and computer programming.
+            </p> */}
         </div>
     )
 }
+
+export default Home
