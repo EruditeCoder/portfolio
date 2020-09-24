406147e2c3865731e235c99496677cd507b2f128
EruditeCoder <marcosaleman84@gmail.com>
2020-09-24 18:30:54 -0400

    Made the navigation list bold in correspondence with current url

--- src/Components/List.js
+++ src/Components/List.js
@@ -1,24 +1,76 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { 
-    Link
+    Link,
 } from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 
 const List = () => {
+    const history = useHistory()
+    
+    const [home, setHome] = useState("home")
+    const [about, setAbout] = useState("about me")
+    const [projects, setProjects] = useState("projects")
+
+    useEffect(() => {
+        switch (window.location.pathname) {
+            case "/":
+                setHome(<b>home</b>)
+                setAbout("about me")
+                setProjects("projects")
+                break
+            case "/about-me":
+                setHome("home")
+                setAbout(<b>about me</b>)
+                setProjects("projects")
+                break
+            case "/projects":
+                setHome("home")
+                setAbout("about me")
+                setProjects(<b>projects</b>)
+                break
+            default:
+                break
+        }
+        return history.listen((location) => {
+            switch (location.pathname) {
+                case "/":
+                    setHome(<b>home</b>)
+                    setAbout("about me")
+                    setProjects("projects")
+                    break
+                case "/about-me":
+                    setHome("home")
+                    setAbout(<b>about me</b>)
+                    setProjects("projects")
+                    break
+                case "/projects":
+                    setHome("home")
+                    setAbout("about me")
+                    setProjects(<b>projects</b>)
+                    break
+                default:
+                    break
+            }
+        })
+
+        
+    }, [history])
+
     return (
         <ul className="text-xl text-center flex flex-col justify-between h-full pt-6 pb-6">
             <li>
                 <Link to="/">
-                    home
+                    {home}
                 </Link>
             </li>
             <li>
                 <Link to="/about-me">
-                    about me
+                    {about}
                 </Link>
             </li>
             <li>
                 <Link to="/projects">
-                    projects
+                    {projects}
                 </Link>
             </li>
         </ul>
--- src/Views/About.js
+++ src/Views/About.js
@@ -2,18 +2,18 @@ import React from 'react'
 
 const About = () => {
     return (
-        <div>
-            <p className="text-xl text-center pt-2">
-                Looking to work with others who have the same obsession with constant 
-                improvement and that love to find new, innovative ways, to solve problems.
+        <div className="text-center">
+            <p className="text-xl">
+                Looking to work with others who have a passion with constantly improving
+                themselves and that love to find new, innovative ways, to solve problems.
             </p>
-            <p className="text-xl text-center pt-2">
+            <p className="text-xl pt-4">
                 I have a fascination for knowledge and, thus, spend a lot of my time learning 
                 about many different kinds of topics from <i> computer programming</i> (<b>JavaScript </b>
                 [React & Next], <b>PHP</b> [Laravel], & <b> MongoDB</b>), <i>networking</i>, 
                 <i> information security</i>, and <i> cloud computing</i> along with other areas of interest 
                 such as, psychology, philosophy, and the factors that impact productivity, efficiency, learning, 
-                and behaviour.
+                and behavior.
             </p>
         </div>
     )
