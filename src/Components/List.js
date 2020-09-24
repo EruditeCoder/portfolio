a8164e4ace7084365c0dda7d2c09252754f6289f
EruditeCoder <marcosaleman84@gmail.com>
2020-09-24 18:35:59 -0400

    Minor code changes

--- src/Components/List.js
+++ src/Components/List.js
@@ -10,27 +10,22 @@ const List = () => {
     const [home, setHome] = useState("home")
     const [about, setAbout] = useState("about me")
     const [projects, setProjects] = useState("projects")
-
-    useEffect(() => {
+    
+    useEffect(() => {        
         switch (window.location.pathname) {
             case "/":
                 setHome(<b>home</b>)
-                setAbout("about me")
-                setProjects("projects")
                 break
             case "/about-me":
-                setHome("home")
                 setAbout(<b>about me</b>)
-                setProjects("projects")
                 break
             case "/projects":
-                setHome("home")
-                setAbout("about me")
                 setProjects(<b>projects</b>)
                 break
             default:
                 break
         }
+
         return history.listen((location) => {
             switch (location.pathname) {
                 case "/":
@@ -52,7 +47,6 @@ const List = () => {
                     break
             }
         })
-
         
     }, [history])
 
