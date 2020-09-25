aeafe1ecf6ee52b7ffa5dec8e9ab280160c3018e
EruditeCoder <marcosaleman84@gmail.com>
2020-09-24 20:49:41 -0400

    Added onClick events to the links (home, about, projects)

--- src/Components/List.js
+++ src/Components/List.js
@@ -1,70 +1,25 @@
-import React, { useEffect, useState } from 'react'
-import { 
-    Link,
-} from 'react-router-dom'
-import { useHistory } from 'react-router-dom'
+import React from 'react'
+import { Link } from 'react-router-dom'
+import ListToggle from './ListToggle'
 
 const List = () => {
-    const history = useHistory()
     
-    const [home, setHome] = useState("home")
-    const [about, setAbout] = useState("about me")
-    const [projects, setProjects] = useState("projects")
-    
-    useEffect(() => {        
-        switch (window.location.pathname) {
-            case "/":
-                setHome(<b>home</b>)
-                break
-            case "/about-me":
-                setAbout(<b>about me</b>)
-                break
-            case "/projects":
-                setProjects(<b>projects</b>)
-                break
-            default:
-                break
-        }
-
-        return history.listen((location) => {
-            switch (location.pathname) {
-                case "/":
-                    setHome(<b>home</b>)
-                    setAbout("about me")
-                    setProjects("projects")
-                    break
-                case "/about-me":
-                    setHome("home")
-                    setAbout(<b>about me</b>)
-                    setProjects("projects")
-                    break
-                case "/projects":
-                    setHome("home")
-                    setAbout("about me")
-                    setProjects(<b>projects</b>)
-                    break
-                default:
-                    break
-            }
-        })
-        
-    }, [history])
-
     return (
-        <ul className="text-xl text-center flex flex-col justify-between h-full pt-6 pb-6">
+        <ul className="text-xl text-center flex flex-col justify-between h-full pt-6 pb-6"
+            onClick={(event) => ListToggle(event)}>
             <li>
                 <Link to="/">
-                    {home}
+                    home
                 </Link>
             </li>
             <li>
                 <Link to="/about-me">
-                    {about}
+                    about
                 </Link>
             </li>
             <li>
                 <Link to="/projects">
-                    {projects}
+                    projects
                 </Link>
             </li>
         </ul>
---
+++ src/Components/ListToggle.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ListToggle = (props) => {
+    console.log("hello")
+    console.log(props.target)
+
+}
+
+export default ListToggle
