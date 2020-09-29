af58772c2689e20243a3be20be6b718bad66c974
Nu11 <nu11@pop-os.localdomain>
2020-09-28 22:09:55 -0400

    Modified ListToggle.js

--- src/Components/List.js
+++ src/Components/List.js
@@ -3,25 +3,24 @@ import { Link } from 'react-router-dom'
 import ListToggle from './ListToggle'
 
 const List = () => {
-    
     return (
         <ul className="text-xl text-center flex flex-col justify-between h-full pt-6 pb-6"
             onClick={(event) => ListToggle(event)}>
-            <li>
-                <Link to="/">
+            <Link to="/">
+                <li>
                     home
-                </Link>
-            </li>
-            <li>
-                <Link to="/about-me">
-                    about
-                </Link>
-            </li>
-            <li>
-                <Link to="/projects">
+                </li>
+            </Link>
+            <Link to="/about-me">
+                <li>
+                    about me
+                </li>
+            </Link>
+            <Link to="/projects">
+                <li>
                     projects
-                </Link>
-            </li>
+                </li>
+            </Link>
         </ul>
     )
 }
