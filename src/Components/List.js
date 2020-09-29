3bbacf306d6dd3c4d232ca03a8301b40688f661c
Marcos Aleman <marcosaleman84@gmail.com>
2020-09-29 18:41:48 -0400

    Removed ListToggle.js and changed the navigation menu to be bold in accordance with the url location

--- src/Components/List.js
+++ src/Components/List.js
@@ -1,26 +1,72 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import ListToggle from './ListToggle'
+import React, { useEffect, useState } from 'react'
+import { 
+    Link,
+} from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 
 const List = () => {
+    const history = useHistory()
+    
+    const [home, setHome] = useState("home")
+    const [about, setAbout] = useState("about me")
+    const [projects, setProjects] = useState("projects")
+    
+    useEffect(() => {        
+        switch (window.location.pathname) {
+            case "/":
+                setHome(<b>home</b>)
+                break
+            case "/about-me":
+                setAbout(<b>about me</b>)
+                break
+            case "/projects":
+                setProjects(<b>projects</b>)
+                break
+            default:
+                break
+        }
+
+        return history.listen((location) => {
+            switch (location.pathname) {
+                case "/":
+                    setHome(<b>home</b>)
+                    setAbout("about me")
+                    setProjects("projects")
+                    break
+                case "/about-me":
+                    setHome("home")
+                    setAbout(<b>about me</b>)
+                    setProjects("projects")
+                    break
+                case "/projects":
+                    setHome("home")
+                    setAbout("about me")
+                    setProjects(<b>projects</b>)
+                    break
+                default:
+                    break
+            }
+        })
+        
+    }, [history])
+
     return (
-        <ul className="text-xl text-center flex flex-col justify-between h-full pt-6 pb-6"
-            onClick={(event) => ListToggle(event)}>
-            <Link to="/">
-                <li>
-                    home
-                </li>
-            </Link>
-            <Link to="/about-me">
-                <li>
-                    about me
-                </li>
-            </Link>
-            <Link to="/projects">
-                <li>
-                    projects
-                </li>
-            </Link>
+        <ul className="text-xl text-center flex flex-col justify-between h-full pt-6 pb-6">
+            <li>
+                <Link to="/">
+                    {home}
+                </Link>
+            </li>
+            <li>
+                <Link to="/about-me">
+                    {about}
+                </Link>
+            </li>
+            <li>
+                <Link to="/projects">
+                    {projects}
+                </Link>
+            </li>
         </ul>
     )
 }
