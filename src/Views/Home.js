862f0d3bc164facf8689cfac0ea1ab169668b993
Marcos Aleman <marcosaleman84@gmail.com>
2020-10-09 12:39:09 -0400

    Minor changes

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -3,10 +3,10 @@ import React from 'react'
 const Home = () => {
     return (
         <div className="text-center">
-            <h2 className="text-xl"> Hello, I'm </h2>
-            <h1 className="text-3xl"> MARCOS ALEMAN </h1>
-            <h2 className="text-2xl italic"> Full Stack Developer </h2>
-            <p className="text-xl pt-4"> Full stack web developer & network engineer
+            <h2 className="text-xl">Hello, I'm</h2>
+            <h1 className="text-3xl">MARCOS ALEMAN</h1>
+            <h2 className="text-2xl italic">Full Stack Developer</h2>
+            <p className="text-xl pt-4">Full stack web developer & network engineer
             with a love for minimalism.
             </p>
         </div>
